72ac50fe981a52d3ec6a68a8a37085021a96f1f1
root <root@DESKTOP-L82GENF>
2023-07-28 17:33:49 +0930

    messafge

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -2,14 +2,14 @@
 
 double weighted_average(int array[], int n){
 
-bool state = false;
 if (n < 1)
 {
     return 0;
 }else{
-double count = 0;
+
 double average;
 for (int j = 0; j < n; j++){
+    double count = 0;
     for (int k = 0; k < n; k++){
     if (array[j] == array[k]){
         count++;
@@ -17,7 +17,6 @@ for (int j = 0; j < n; j++){
 }
 double val = array[j];
 average += val * count / n;
-count = 0;
 }
 return(average);
 }
