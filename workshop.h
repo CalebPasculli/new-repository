270d615dc83e66c5c081ae9bc5481f1e06538123
root <root@DESKTOP-L82GENF>
2023-08-08 10:53:03 +0930

    message

--- workshop.h
+++ workshop.h
@@ -2,8 +2,9 @@
 
 void printArray(double* array, int n){
     for (int i = 0; i < n; i++){
-        std::cout << array[i] << "\n";
+        std::cout << array[i] << " ";
     }
+    std::cout <<"\n";
 }
 
 
