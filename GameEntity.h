f21f7cb78e86d2582d30504a967d9ffa9e8d80b8
root <root@DESKTOP-L82GENF>
2023-10-24 22:39:02 +1030

    message

---
+++ GameEntity.h
@@ -0,0 +1,12 @@
+#ifndef GAMEENTITY_H
+#define GAMEENTITY_H
+class GameEntity{
+private:
+std::tuple<int, int> position;
+char type;
+public:
+GameEntity(int x, int y, char type);
+std::tuple<int, int> getPos():;
+char getType()
+};
+#endif
